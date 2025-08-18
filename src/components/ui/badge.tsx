aad1e0867c5c08cd5f8a0ac64e0751b107783485
John Randelle <john.randelle@senso.ph>
2025-08-18 23:41:03 +0800

    feat: added variants to badge.tsx

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -12,10 +12,16 @@ const badgeVariants = cva(
         default: "border-transparent bg-primary text-primary-foreground [a&]:hover:bg-primary/90",
         secondary:
           "border-transparent bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90",
+        outline: "text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground",
+        stone: "border border-stone-300 bg-stone-200 text-stone-900 [a&]:hover:bg-stone-200/90",
+        success:
+          "border-transparent bg-success text-white [a&]:hover:bg-success/90 focus-visible:ring-success/20 dark:focus-visible:ring-success/40 dark:bg-success/60",
+        info: "border-transparent bg-info text-white [a&]:hover:bg-info/90 focus-visible:ring-info/20 dark:focus-visible:ring-info/40 dark:bg-info/60",
+        warning:
+          "border-transparent bg-warning text-warning-foreground [a&]:hover:bg-warning/90 focus-visible:ring-warning/20 dark:focus-visible:ring-warning/40 dark:bg-warning/60",
+
         destructive:
           "border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
-        outline: "text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground",
-        stone: "border-transparent bg-stone-200 text-stone-900 [a&]:hover:bg-stone-200/90",
       },
     },
     defaultVariants: {
